c234628c46512f08f13c9a77617bc7c0bc532e18
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-08-25 19:53:24 -0400

    commit 2

---
+++ src/currying.js
@@ -0,0 +1,7 @@
+function add(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+add(1)(5); // add(1, 5) - currying
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const numbers = [1, 2, 3];
+
+// Adding
+const index = numbers.indexOf(2);
+const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
+
+// Removing
+const removed = numbers.filter((n) => n != 2);
+
+// Updated
+const updated = numbers.map((n) => (n === 2 ? 20 : n));
+console.log(updated);
